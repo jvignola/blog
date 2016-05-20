37b9eee50967cf297a93e598e4c4e76153349801
jvignola <godsavethebufon@hotmail.com>
2016-05-20 09:01:31 -0300

    strong params parciales y edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,8 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def create
-		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		#@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		@article = Article.new(article_params)
 		if @article.save
 			redirect_to @article
 		else
@@ -20,5 +21,22 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render "edit"
+		end
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :body)
 	end
 end
